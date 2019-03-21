92f2e0249c84eb28a26b9f5cad22472d81e9bfc7
yourior <37235534+yourior@users.noreply.github.com>
2019-03-21 17:02:50 +0700

    Add - Move Layout to Setting (Belum ada settingnya, harus pasang sendiri )

--- app/src/main/java/com/example/entre31proto3/MainActivity.java
+++ app/src/main/java/com/example/entre31proto3/MainActivity.java
@@ -33,6 +33,7 @@ public class MainActivity extends AppCompatActivity
 //                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 //                        .setAction("Action", null).show();
 //            }
+        //test
 //        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
