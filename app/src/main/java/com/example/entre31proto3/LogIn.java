eff80461e1b7d6b17dfb79212d431072bb6360e5
yourior <37235534+yourior@users.noreply.github.com>
2019-04-01 16:44:28 +0700

    Done -Register
    Stil need Checking Tho

--- app/src/main/java/com/example/entre31proto3/LogIn.java
+++ app/src/main/java/com/example/entre31proto3/LogIn.java
@@ -4,13 +4,23 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class LogIn extends AppCompatActivity {
 
+    DatabaseRegister dbregister;
+
+    EditText UserName ;
+    EditText Password ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        UserName = (EditText) findViewById(R.id.Username_Login);
+        Password = (EditText) findViewById(R.id.Password_Login);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        dbregister= new DatabaseRegister(this);
     }
 
         public void MoveToRegister2(View view)
@@ -19,4 +29,22 @@ public class LogIn extends AppCompatActivity {
 
         startActivity(intent);
     }
+
+    public void Login(View view)
+    {
+        String user = UserName.getText().toString().trim();
+        String pwd = Password.getText().toString().trim();
+
+        Boolean Check = dbregister.CheckUser(user,pwd);
+
+        if(Check)//Success
+        {
+
+        }else if(!Check) //Failed
+        {
+
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/entre31proto3/Register2.java
+++ app/src/main/java/com/example/entre31proto3/Register2.java
@@ -1,6 +1,6 @@
 package com.example.entre31proto3;
 
-import android.provider.BaseColumns;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,20 +14,20 @@ public class Register2 extends AppCompatActivity {
     EditText Password ;
     EditText ConPassword ;
     TextView Info ;
+    DatabaseRegister dbregister;
 
-    //DatabaseRegister dbregister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        //dbregister= new DatabaseRegister(this);
+        dbregister = new DatabaseRegister(this);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register2);
 
-        UserName = findViewById(R.id.Username);
+        UserName = findViewById(R.id.Username_Login);
 
-        Password = findViewById((R.id.Password));
+        Password = findViewById((R.id.Password_Login));
 
         ConPassword =  findViewById((R.id.Confirm_Password));
         Info = findViewById(R.id.Info2);
@@ -36,9 +36,22 @@ public class Register2 extends AppCompatActivity {
 
     public void RegisterClick(View view)
     {
-        if(Password.equals(ConPassword)) //true
+        String user = UserName.getText().toString().trim();
+        String pwd = Password.getText().toString().trim();
+        String conpwd = ConPassword.getText().toString().trim();
+        if(pwd.equals(conpwd)) //true
         {
-            Info.setText("It's a Match");
+            long val =dbregister.addUser(user,pwd);
+            if(val>0)
+            {
+                Info.setText("It's a Match");
+
+                Intent intent = new Intent(this, LogIn.class);
+                startActivity(intent);
+            }else
+            {
+                Info.setText("Registration Error");
+            }
         }
         else // false
         {
